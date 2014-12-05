0374e70c7cbd91b40d2de1b93a477cf21e48b19e
Yosuke Mitsui <yosuke-mitsui@gmo.jp>
2014-12-05 12:55:37 +0900

    change .a to .so

--- dpgmm.c
+++ dpgmm.c
@@ -48,7 +48,7 @@ void dpgmm_release(DPGMM *ctx){
 	gsl_matrix_free(ctx->z);
 	free(ctx);
 }
-void dpgmm_add(DPGMM *ctx,double *sample){	
+void dpgmm_add(DPGMM *ctx,double *sample){
 	memcpy(&ctx->data[ctx->numData*ctx->dims],sample,sizeof(double)*ctx->dims);
 	ctx->numData++;
 	if((ctx->numData%LIMIT_DATA)==0){
